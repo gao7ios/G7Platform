a13986e53f552df09ef173f4a6e50e920dce6943
余洋 <yuyangmx2@gmail.com>
2016-01-19 14:39:56 +0800

    删除邮箱密码

--- G7Platform/G7Platform/main/site/Application/G7ApplicationReqHandlers.py
+++ G7Platform/G7Platform/main/site/Application/G7ApplicationReqHandlers.py
@@ -123,10 +123,10 @@ class G7ApplicationPgyerUploader():
 			mail_username = self.currentG7User.username
 			mail_pwd = self.currentG7User.mail_pwd
 		else:
-			mail_host = 'smtp.vip.163.com'
-			mail_user = 'yumous@vip.163.com'
-			mail_username = 'yumous'
-			mail_pwd = 'JIM@19871017'
+			mail_host = 'test'
+			mail_user = 'test'
+			mail_username = 'test'
+			mail_pwd = 'test'
 
 		mail_to = ','.join(mail_receiver)
 		mail_smtpPort = '25'
