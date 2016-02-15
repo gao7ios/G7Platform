52eaaeb979ab0471ba44c5d03fa94ddf3517667d
余洋 <yuyangmx2@gmail.com>
2016-02-15 17:16:33 +0800

    修改设置

--- G7Platform/G7Platform/profile/settings/G7Settings.py
+++ G7Platform/G7Platform/profile/settings/G7Settings.py
@@ -27,7 +27,7 @@ tornado_ports = [
 ######################## 项目 ###########################
 
 # 项目名
-project_name = "G7Platform";
+project_name = "BMPlatform";
 
 # 后台名
 django_project_name="g7admin"
@@ -50,8 +50,6 @@ media_path = path.join(project_path,"media")
 # django 路径
 django_path = path.join(subproject_path, "main/"+django_project_name)
 
-# django管理器路径
-django_manage_path = path.join(django_path,"manage.py")
 
 ####################### 部署 ###########################
 
@@ -71,12 +69,42 @@ log_path = path.join(workspace_path, "log")
 nginx_path = path.join(profile_path, "nginx")
 
 # nginx 主配置文件路径
+import platform
+osname = platform.uname()[0]
 nginx_conf_path = path.join(nginx_path, "nginx.conf")
+if osname == "Darwin":
+	nginx_conf_path = path.join(nginx_path, "nginx_bsd.conf")
 
-# nginx G7配置文件路径
+# nginx BM配置文件路径
 nginx_g7_conf_path = path.join(nginx_path, project_name.lower())
 
 tornado_log_path = path.join(log_path,"tornado/tornado.log")
+
+# django管理器路径
+django_manage_path = path.join(django_path,"manage.py")
+
+def g7MakeDirs(path_name, touch_file=False):
+	if len(os.path.splitext(path_name)[1]) > 0:
+		if os.path.exists(os.path.dirname(path_name)) == False:
+			os.makedirs(os.path.dirname(path_name), exist_ok=True)
+		if touch_file == True and os.path.exists(path_name) == False:
+			os.system("touch {path_name}".format(path_name=path_name))
+	else:
+		if os.path.exists(path_name) == False:
+			os.makedirs(path_name, exist_ok=True)
+
+g7MakeDirs(project_path)
+g7MakeDirs(subproject_path)
+g7MakeDirs(template_path)
+g7MakeDirs(static_path)
+g7MakeDirs(media_path)
+g7MakeDirs(django_path)
+g7MakeDirs(workspace_path)
+g7MakeDirs(profile_path)
+g7MakeDirs(shell_path)
+g7MakeDirs(log_path)
+g7MakeDirs(nginx_path)
+g7MakeDirs(tornado_log_path, touch_file=True)
 
 ########################### 数据库 ###########################
 
@@ -92,8 +120,7 @@ else:
 	dbuser = "dg7platform_user"
 	dbpassword = "dg7platform_password"
 
-########################### 其他 #############################
 
-desPassword = "12345678"
+######################## 其他 ###############################
 
-
+desPassword = "bfae43b0"
