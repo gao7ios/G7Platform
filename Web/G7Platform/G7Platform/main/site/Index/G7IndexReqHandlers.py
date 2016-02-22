a19e5f8e57d03a546343ce11173bc9b17660c8c9
余洋 <yuyangmx2@gmail.com>
2016-02-22 11:00:49 +0800

    添加页面

--- Web/G7Platform/G7Platform/main/site/Index/G7IndexReqHandlers.py
+++ Web/G7Platform/G7Platform/main/site/Index/G7IndexReqHandlers.py
@@ -4,10 +4,27 @@ from G7Platform.main.site.Common.G7APIReqHandlers import G7APIReqHandler
 from G7Platform.main.site.Common.G7WebReqHandlers import G7WebReqHandler
 
 class G7IndexReqHandler(G7WebReqHandler):
+	'''
+		首页
+	'''
+	def get(self):
+		return self.render("index/index.html", title="首页")
+
+
+class G7BlogReqHandler(G7WebReqHandler):
 
 	def get(self):
+		return self.render("index/blog.html", title="博客")
 
-		return self.render("test/test.html", title="测试")
+class G7ContactReqHandler(G7WebReqHandler):
+
+	def get(self):
+		return self.render("index/Contact.html", title="联系方式")
+
+class G7SingleReqHandler(G7WebReqHandler):
+
+	def get(self):
+		return self.render("index/single.html", title="个人")
 
 
 class G7AppIndexReqHandler(G7WebReqHandler):
--- Web/G7Platform/G7Platform/profile/settings/web/G7URLs.py
+++ Web/G7Platform/G7Platform/profile/settings/web/G7URLs.py
@@ -5,21 +5,28 @@ from G7Platform.main.site.Account.G7AccountReqHandlers import *
 from G7Platform.main.site.Index.G7IndexReqHandlers import *
 from G7Platform.main.site.Application.G7ApplicationReqHandlers import *
 
-version = 1.0
+# 接口url配置方法
+def G7ApiPathURL(channel, path, className, version=None):
 
-def G7ApiPathURL(channel, path, className):
-    return (r"/api/{version}/{channel}/{path}".format(version=version, channel=channel, path=path), className)
+    if version == None:
+        return (r"/api/{channel}/{path}".format(channel=channel, path=path), className)
+    else:
+        return (r"/api/{version}/{channel}/{path}".format(version=version, channel=channel, path=path), className)
 
 # url配置信息
 
+# 配置web地址列表
 webURLList = [
 	    (r"/", G7IndexReqHandler),
+        (r"/blog", G7BlogReqHandler),
+        (r"/contact", G7ContactReqHandler),
+        (r"/single", G7SingleReqHandler),
         (r"/app/", G7AppIndexReqHandler),
 ]
 
+# 配置接口列表
 apiURLList = [
-    G7ApiPathURL("application","upload",G7ApplicationReqHandler),
+    G7ApiPathURL("application","upload",G7ApplicationReqHandler, version="1.0"),
 ]
-
 
 urlList = apiURLList+webURLList
