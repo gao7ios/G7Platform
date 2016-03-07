671fdb20e2a04c88899ad231a8edd0f937634f7c
余洋 <yuyangmx2@gmail.com>
2016-03-07 18:01:53 +0800

    修改脚本

--- Apple/MarsX/MarsX/Library/G7BLL/G7BLL.framework/Versions/A/Headers/G7Macro.h
+++
@@ -1,9 +0,0 @@
-//
-//  G7Macro.h
-//  Gao7PadTool
-//
-//  Created by WangMingfu on 14/7/21.
-//  Copyright (c) 2014年 Tandy. All rights reserved.
-//
-
-#import "G7QuickMacro.h"
--- Apple/MarsX/MarsX/Library/G7Core/G7Core.framework/Versions/A/Headers/G7UI.h
+++
@@ -1,9 +0,0 @@
-//
-//  G7UI.h
-//  G7Core
-//
-//  Created by WangMingfu on 15/10/27.
-//  Copyright © 2015年 Tandy. All rights reserved.
-//
-
-
