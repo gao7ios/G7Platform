a0e9f522a513a83a7714ac6fa80bec05862d24a7
passionlzx <luzhx@luzhxtekiMacBook-Pro.local>
2016-03-19 16:23:12 +0800

    反馈后台修改

---
+++ Web/G7Platform/G7Platform/main/g7admin/Feedback/__init__.py
@@ -0,0 +1,3 @@
+#-*- coding: UTF-8 -*-
+
+default_app_config = "Feedback.apps.FeedbackConfig"
--- Web/G7Platform/G7Platform/main/g7admin/Feedback/admin.py
+++ Web/G7Platform/G7Platform/main/g7admin/Feedback/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 from Feedback.models import G7FeedbackModel
 
-# Register your models here.
 class G7FeedbackAdmin(admin.ModelAdmin):
-    # 设置为pass 代表着这个类只是继承了父类，并没有添加或者重写任何东西，同时意味着django会自动帮你解析G7Feedback中所定义的那些属性
     pass
 
-# 连接模型和G7FeedbackAdmin
 admin.site.register(G7FeedbackModel, G7FeedbackAdmin)
