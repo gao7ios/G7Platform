6dc865c29231b8d1f1552dcfbf436fd187846549
passionlzx <luzhx@luzhxtekiMacBook-Pro.local>
2016-03-28 21:07:00 +0800

    反馈接口

--- Web/G7Platform/G7Platform/main/site/Feedback/G7FeedbackReqHandlers.py
+++ Web/G7Platform/G7Platform/main/site/Feedback/G7FeedbackReqHandlers.py
@@ -2,13 +2,23 @@
 
 from G7Platform.main.site.Common.G7APIReqHandlers import G7APIReqHandler
 from G7Platform.main.site.Common.G7WebReqHandlers import G7WebReqHandler
+from Feedback.models import G7FeedbackModel
 
 class G7FeedbackReqHandler(G7APIReqHandler):
 
-    def post(self):
-        resultData = {"requestData":self.paramsJson, "User-Agent1":self.httpHeadersJson}
-        g7log(resultData)
-        self.responseWrite(0, "asdfasdf2", data=resultData)
+    def get(self):
+        username = self.paramsJson.get("username")
+        contact = self.paramsJson.get("contact")
+        context = self.paramsJson.get("context")
+        try:
+                feedback = G7FeedbackModel(username=username, contact=contact, context=context)
+                feedback.save()
+                self.responseWrite(0, "提交成功", data={})
+        except:
+                self.responseWrite(1, "提交失败", data={})
+		
+
+        
 
         	
 
--- Web/G7Platform/G7Platform/profile/settings/web/G7URLs.py
+++ Web/G7Platform/G7Platform/profile/settings/web/G7URLs.py
@@ -6,6 +6,7 @@ from G7Platform.main.site.Index.G7IndexReqHandlers import *
 from G7Platform.main.site.Application.G7ApplicationReqHandlers import *
 from G7Platform.main.site.Application.G7AppInstallReqHandlers import *
 from G7Platform.main.site.G7Test.G7TestReqHandlers import *
+from G7Platform.main.site.Feedback.G7FeedbackReqHandlers import *
 
 # 接口url配置方法
 def G7ApiPathURL(channel, path, className, version=None):
@@ -30,6 +31,8 @@ webURLList = [
 apiURLList = [
     G7ApiPathURL("application","upload",G7ApplicationReqHandler, version="1.0"),
     (r"/test", G7TestReqHandler),
+    (r"/feedback", G7FeedbackReqHandler),
+
 
     (r"/application/install/(?P<app_id>.*?).html", G7AppInstallReqHandler),
     (r"/application/install/(?P<plist_app_id>.*?).plist", G7AppPlistReqHandler),
