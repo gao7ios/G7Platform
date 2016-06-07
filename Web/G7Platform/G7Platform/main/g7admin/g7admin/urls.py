a57b76129a3385d750e6565c8f279cbe7dd233b6
余洋 <yuyangmx2@gmail.com>
2016-06-07 18:32:12 +0800

    静态文件配置修改

--- Web/G7Platform/G7Platform/main/g7admin/g7admin/urls.py
+++ Web/G7Platform/G7Platform/main/g7admin/g7admin/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
